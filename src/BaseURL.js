fcc2db8c3ad7ba1a861b74625be98d1e852fb89d
Malfry <okseries211295@gmail.com>
2024-03-20 16:07:18 -0400

    usare esto como un barril para gestionar mis urls

---
+++ src/BaseURL.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+const storedToken = localStorage.getItem('accessToken');
+const { idSucursal } = jwt.decode(storedToken);
+
+const baseURL = 'http://localhost:8080/api/v1';
+const sucursalURL = `/sucursal/${idSucursal}`;
+
+export const GenerarCuotaURL = `${baseURL}/cuotas`;
+export const CrearClienteURL = `${baseURL}/clientes${sucursalURL}`;
+export const ListaPrestamoURL = `${baseURL}/prestamos${sucursalURL}`;
+export const LoginURL = `${baseURL}/usuarios${sucursalURL}`;
+export const ListarUsuariosURL = `${baseURL}/usuarios${sucursalURL}`;
+export const ListarClientesURL = `${baseURL}/clientes${sucursalURL}`;
