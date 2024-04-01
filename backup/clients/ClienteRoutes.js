02300837fbf05ddc0595e4dfa7cce691629e369b
Malfry <okseries211295@gmail.com>
2024-04-01 14:39:05 -0400

    Elimine la ruta del formulario que se usaba antes para crear un cliente

--- backup/clients/ClienteRoutes.js
+++ backup/clients/ClienteRoutes.js
@@ -2,16 +2,11 @@ import Loadable from 'app/components/Loadable';
 import { lazy } from 'react';
 
 const ClientList = Loadable(lazy(() => import('./ClientList')));
-const ClienteCreate = Loadable(lazy(() => import('./ClienteCreate')));
 
 const ClienteRoutes = [
   {
     path: '/cliente-list',
     element: <ClientList />,
-  },
-  {
-    path: '/cliente-create',
-    element: <ClienteCreate />,
   },
 ];
 
